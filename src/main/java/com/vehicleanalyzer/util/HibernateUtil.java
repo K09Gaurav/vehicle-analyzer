5a94d91d3cf5c5d44c986e7ede499cfd68e387d4
Kumar Gaurav <k09gaurav@gmail.com>
2025-04-20 14:01:38 +0530

    Integrated and Tested PostgreSQL with Hibernate

--- src/main/java/com/vehicleanalyzer/util/HibernateUtil.java
+++ src/main/java/com/vehicleanalyzer/util/HibernateUtil.java
@@ -1,7 +1,11 @@
 package com.vehicleanalyzer.util;
 
+
+import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 public class HibernateUtil {
     private static SessionFactory sessionFactory;
@@ -12,12 +16,36 @@ public class HibernateUtil {
             try {
                 Configuration configuration = new Configuration();
                 configuration.configure("hibernate.cfg.xml");
-            } catch (Exception e) {
-                // TODO: handle exception
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties())
+                        .build();
+                
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+
+
+            } catch (HibernateException e) {
+                System.err.println("SessionFactory creation failed: " + e.getMessage());
+                throw new ExceptionInInitializerError(e);
             }
         }
-
-
         return sessionFactory;
     }
+
+    public static void shutdown() {
+        if (sessionFactory != null) {
+            sessionFactory.close();
+        }
+    }
+    
+    // public static void main(String[] args) {
+    //     // Test the HibernateUtil class
+    //     SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+    //     if (sessionFactory != null) {
+    //         System.out.println("SessionFactory created successfully!");
+    //     } else {
+    //         System.out.println("Failed to create SessionFactory.");
+    //     }
+    // }
 }
